fe97f90d90752a6f956c1848d5ca0be2c1a28ae7
pjay94 <pjayit94@gmail.com>
2024-05-06 21:49:36 +0200

    7 - db id fix (wrong primary key error)

--- src/main/java/com/library/model/Genre.java
+++ src/main/java/com/library/model/Genre.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 @Table(name = "genres")
 public class Genre {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
